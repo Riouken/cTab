c38bbe8c5177a548e3a68695b51a653620c70710
nsgundy <nils.lichtenfeld@gmx.net>
2014-06-05 23:28:40 +0200

    Adding cTab_ico.paa and mod.cpp from original cTab v1. Addressing #16.

---
+++ @cTab/mod.cpp
@@ -0,0 +1,5 @@
+name = "Commander's Tablet / FBCB2 - Blue Force Tracking";
+picture = "cTab_ico.paa";
+actionName = "Website";
+action = "http://forums.bistudio.com/member.php?64032-Riouken";
+description = "BI Thread: http://forums.bistudio.com/showthread.php?163030-WIP-cTab-Commander-s-Tablet-FBCB2-Bluforce-Tracker<br />";
