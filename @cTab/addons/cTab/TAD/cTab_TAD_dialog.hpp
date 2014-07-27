1d967cb021f2623091dda1358e26f2e50485d54b
nsgundy <nils.lichtenfeld@gmx.net>
2014-07-27 22:43:25 +0200

    Change OSD element for icon text toggle status to be 3 characters wide and show no text when initialized

--- @cTab/addons/cTab/TAD/cTab_TAD_dialog.hpp
+++ @cTab/addons/cTab/TAD/cTab_TAD_dialog.hpp
@@ -133,11 +133,11 @@ class cTab_TAD_dialog
 			};
 			class on_screen_toggleText2: cTab_RscText_TAD
 			{
-				x = (cTab_GUI_TAD_OSD_EDGE_R - cTab_GUI_TAD_OSD_OSB_TEXT_OFFSET - cTab_GUI_TAD_OSD_ELEMENT_STD_W * 2) / GUI_GRID_PX_W * GUI_GRID_W + GUI_GRID_X;
 				idc = IDC_CTAB_OSD_TXT_TGGL;
+				x = (cTab_GUI_TAD_OSD_EDGE_R - cTab_GUI_TAD_OSD_OSB_TEXT_OFFSET - cTab_GUI_TAD_OSD_ELEMENT_STD_W * 3) / GUI_GRID_PX_W * GUI_GRID_W + GUI_GRID_X;
 				y = (cTab_GUI_TAD_OSD_OSB10_Y) / GUI_GRID_PX_W * GUI_GRID_H + GUI_GRID_Y;
-				w = cTab_GUI_TAD_OSD_ELEMENT_STD_W * 2 / GUI_GRID_PX_H * GUI_GRID_W;
-				text = "ON";
+				w = cTab_GUI_TAD_OSD_ELEMENT_STD_W * 3 / GUI_GRID_PX_H * GUI_GRID_W;
+				text = "";
 			};
 
 
